a092b2d8519de4572ac365b07b2fbca7ee37207d
DESKTOP-U3DONJ4\yukia <neron.anyu@gmail.com>
2022-02-28 10:30:54 +0900

    画面サイズを全画面にでタブのスクロールがなくなるように調整

--- sketch.js
+++ sketch.js
@@ -1,146 +1,156 @@
-window.addEventListener('load', eventWindowLoaded, false);
-let canvas = document.querySelector('canvas');
-let ctx = canvas.getContext('2d');
+window.addEventListener("load", eventWindowLoaded, false);
+let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
 
-let cursors = new Array(9);//0:リアルカーソル, 0以外: ダミーカーソル
+let cursors = new Array(9); //0:リアルカーソル, 0以外: ダミーカーソル
 let windowLeft = 0;
 let movementX = 0;
 let movementY = 0;
 let cursorSize = [1, 10, 189, 377, 566, 754, 1080];
 
 function eventWindowLoaded() {
-    setup();
-    draw();
+  setup();
+  draw();
 }
 
 function canvasLoop(e) {
-    movementX = e.movementX || e.mozMovementX || 0;
-    movementY = e.movementY || e.mozMovementY || 0;
-    draw();
+  movementX = e.movementX || e.mozMovementX || 0;
+  movementY = e.movementY || e.mozMovementY || 0;
+  draw();
 }
 
 function setup() {
-    canvas.width = window.outerWidth;
-    canvas.height = window.outerHeight;
-    windowLeft = (canvas.width - canvas.height) / 2;
-    initCursors();
+  vel = 0.99;
+  canvas.width = window.outerWidth * vel;
+  canvas.height = window.outerHeight * (vel - 0.08);
+  windowLeft = ((canvas.width - canvas.height) * vel) / 2;
+  initCursors();
 }
 
-
 function draw() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-    ctx.fillStyle = "white";
-    ctx.fillRect(windowLeft, 0, canvas.width - 2 * windowLeft, canvas.clientHeight);
-    ctx.fill();
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+  ctx.fillStyle = "white";
+  ctx.fillRect(
+    windowLeft,
+    0,
+    canvas.width - 2 * windowLeft,
+    canvas.clientHeight
+  );
+  ctx.fill();
 
-    for (let i = 0; i < cursors.length; i++) {
-        let c = cursors[i];
-        c.update(movementX, movementY);
-        c.checkEdges();
-        c.display(ctx);
-    }
+  for (let i = 0; i < cursors.length; i++) {
+    let c = cursors[i];
+    c.update(movementX, movementY);
+    c.checkEdges();
+    c.display(ctx);
+  }
 
-    ctx.font = "18px NotoSans";
-    ctx.fillStyle = "white";
-    ctx.fillText("キーボード操作", 10, 50);
-    ctx.fillText("r:自身のカーソルを表示", 10, 80);
-    ctx.fillText("s:自身のカーソルを表示", 10, 110);
-    ctx.fillText("1~" + String(cursorSize.length) + ":カーソルの大きさを変更", 10, 140);
+  ctx.font = "18px NotoSans";
+  ctx.fillStyle = "white";
+  ctx.fillText("キーボード操作", 10, 50);
+  ctx.fillText("r:リセット", 10, 80);
+  ctx.fillText("s:自身のカーソルを表示", 10, 110);
 
-    ctx.fillText("※実際の実験環境とは異なります", 10, canvas.height - 200);
+  ctx.fillText(
+    "1~" + String(cursorSize.length) + ":カーソルの大きさを変更",
+    10,
+    140
+  );
+
+  ctx.fillText("※実際の実験環境とは異なります", 10, canvas.height - 200);
 }
 
 function initCursors() {
-    for (let i = 0; i < cursors.length; i++) {
+  for (let i = 0; i < cursors.length; i++) {
+    let x = random(canvas.width);
+    let y = random(canvas.height);
+    let c = new Cursor(x, y);
 
-        let x = random(canvas.width);
-        let y = random(canvas.height);
-        let c = new Cursor(x, y);
-
-        if (i == 0) {
-            c.moveRad = degToRad(0);
-        } else {
-            c.moveRad = degToRad(45 + i * 30);
-        }
-        c.id = i;
-        c.xMin = windowLeft;
-        c.xMax = canvas.width - windowLeft;
-        c.yMin = 0;
-        c.yMax = canvas.height;
-        c.size = 10;
-
-        cursors[i] = c;
+    if (i == 0) {
+      c.moveRad = degToRad(0);
+    } else {
+      c.moveRad = degToRad(45 + i * 30);
     }
+    c.id = i;
+    c.xMin = windowLeft;
+    c.xMax = canvas.width - windowLeft;
+    c.yMin = 0;
+    c.yMax = canvas.height;
+    c.size = 10;
+
+    cursors[i] = c;
+  }
 }
 
 /*
  *キーボード操作
  */
-document.addEventListener('keydown', (event) => {
+document.addEventListener(
+  "keydown",
+  (event) => {
     const keyName = event.key;
 
     //カーソルリセット
-    if (keyName === 'r') {
-        initCursors();
+    if (keyName === "r") {
+      initCursors();
     }
 
     //リアルカーソルだけ赤くする
-    if (keyName === 's') {
-        cursors[0].showCursor = true;
+    if (keyName === "s") {
+      cursors[0].showCursor = true;
     }
 
     for (let i = 0; i < cursorSize.length; i++) {
-        if (keyName === String(i + 1)) {
-            cursors.forEach(function (cursor) {
-                cursor.size = cursorSize[i];
-            })
-
-        }
+      if (keyName === String(i + 1)) {
+        cursors.forEach(function (cursor) {
+          cursor.size = cursorSize[i];
+        });
+      }
     }
 
-
     draw();
-}, false);
-
+  },
+  false
+);
 
 /*
  *カーソル固定
  */
-canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock;
-document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock;
+canvas.requestPointerLock =
+  canvas.requestPointerLock || canvas.mozRequestPointerLock;
+document.exitPointerLock =
+  document.exitPointerLock || document.mozExitPointerLock;
 
 canvas.onclick = function () {
-    canvas.requestPointerLock();
-}
+  canvas.requestPointerLock();
+};
 
-
-document.addEventListener('pointerlockchange', lockChangeAlert, false);
-document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
+document.addEventListener("pointerlockchange", lockChangeAlert, false);
+document.addEventListener("mozpointerlockchange", lockChangeAlert, false);
 
 function lockChangeAlert() {
-    if (document.pointerLockElement === canvas ||
-        document.mozPointerLockElement === canvas) {
-        console.log('The pointer lock status is now locked');
-        document.addEventListener("mousemove", canvasLoop, false);
-    } else {
-        console.log('The pointer lock status is now unlocked');
-        document.removeEventListener("mousemove", canvasLoop, false);
-    }
+  if (
+    document.pointerLockElement === canvas ||
+    document.mozPointerLockElement === canvas
+  ) {
+    console.log("The pointer lock status is now locked");
+    document.addEventListener("mousemove", canvasLoop, false);
+  } else {
+    console.log("The pointer lock status is now unlocked");
+    document.removeEventListener("mousemove", canvasLoop, false);
+  }
 }
-
-
-
 
 /*
  *数式関係
  */
 
 function degToRad(degrees) {
-    let result = Math.PI / 180 * degrees;
-    return result;
+  let result = (Math.PI / 180) * degrees;
+  return result;
 }
 
 function random(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
